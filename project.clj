59ccc0096393f35542918a60e796e6839c984bd2
Scott Richmond <kredati@users.noreply.github.com>
2022-03-20 17:41:26 -0400

    Add cljfmt to lein project

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
+  :plugins [[lein-cljfmt "0.8.0"]]
   :repl-options {:init-ns ludus.core}
   :main ludus.core)
