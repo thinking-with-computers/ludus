b277a1bf2aa639a6f2d14591dc70ab25823e379a
Scott Richmond <kredati@users.noreply.github.com>
2022-04-30 13:55:52 -0400

    Add clojurescript

--- project.clj
+++ project.clj
@@ -3,9 +3,22 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :plugins [[lein-cljfmt "0.8.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.11.1"]
+    [org.clojure/clojurescript "1.11.4"]
+  ]
+  :plugins [
+    [lein-cljfmt "0.8.0"]
+    [lein-cljsbuild "1.1.8"]
+  ]
   :repl-options {:init-ns ludus.core}
   :main ludus.core
   :profiles {:uberjar {:aot :all}}
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src/ludus"]
+        :compiler {
+          :output-to "target/js/main.js"
+          :optimizations :none
+          :pretty-print true}}]}
   )
