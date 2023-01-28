b3e970933bbba91f57e832443934770dfaec4244
Scott Richmond <kredati@users.noreply.github.com>
2023-01-28 17:48:43 -0500

    fix graal compile opts for vthreads

--- graal-compile.sh
+++ graal-compile.sh
@@ -4,4 +4,4 @@ jenv shell graalvm64-19.0.2
 
 lein uberjar
 
-native-image --report-unsupported-elements-at-runtime --initialize-at-build-time -jar ./target/ludus-0.1.0-SNAPSHOT-standalone.jar -H:Name=./target/ludus
+native-image --enable-preview --report-unsupported-elements-at-runtime --initialize-at-build-time -jar ./target/ludus-0.1.0-SNAPSHOT-standalone.jar -H:Name=./target/ludus
