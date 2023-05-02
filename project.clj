197f4772ba8557e476a1dc87088877b5488ca86c
Scott Richmond <s.richmond@utoronto.ca>
2023-05-02 19:08:18 -0400

    keep unfucking merge

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [babashka/fs "0.1.6"]
                  [quil "4.0.0-SNAPSHOT-1"]]
->>>>>>> 55d76f6854bf67119873d98e2c9c18d8390ab90a
   :plugins [[lein-cljfmt "0.8.0"]]
   :repl-options {:init-ns ludus.core}
   :main ludus.core
