e02e972d27c95e199cffc723ace7af5384e51df7
Scott Richmond <s.richmond@utoronto.ca>
2023-05-20 14:25:33 -0400

    Start work on a compiler

--- src/ludus/compile.clj
+++ src/ludus/compile.clj
@@ -3,3 +3,11 @@
   		[ludus.grammar :as g]
   		[ludus.parser-new :as p]
   		[ludus.scanner :as s]))
+
+(def source 
+  "1"
+  )
+
+(def result (->> source s/scan :tokens (p/apply-parser g/script)))
+
+(println result)
