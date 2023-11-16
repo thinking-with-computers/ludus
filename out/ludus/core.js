e276298f4e27e68d48e4c6fa2df27bc9df284b28
Scott Richmond <kredati@users.noreply.github.com>
2023-11-16 13:24:47 -0500

    Don't track out

--- out/ludus/core.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.11.121 {:optimizations :none}
-goog.provide('ludus.core');
-goog.require('cljs.core');
-cljs.core.println.call(null,"Hello, world!");
-
-//# sourceMappingURL=core.js.map
